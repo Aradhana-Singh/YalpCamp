31653bbb3a7d1f06e599f7b57058a2e0099a7fb0
Aradhana Singh <aradhana971127@gmail.com>
2020-05-18 20:54:58 +0530

    commit

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+var campground = [
+    {
+        name : "Gorakhpur",
+        image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=500&q=80"
+    },
+    {
+        name : "Ranchi",
+        image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
+    },
+    {
+        name : "Delhi",
+        image: "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
+    }
+];
 
 app.get('/',function(req, res){
     res.render("landing");
 });
 
 app.get('/campgrounds',function(req,res){
-    var campground = [
-        {
-            name : "Gorakhpur",
-            image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=500&q=80"
-        },
-        {
-            name : "Ranchi",
-            image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
-        },
-        {
-            name : "Delhi",
-            image: "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
-        }
-    ]
     res.render("campgrounds",{campground:campground});
 });
 
+app.get('/campgrounds/new',function(req,res){
+    res.render("new");
+});
+app.post('/campgrounds',function(req,res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var camps = {name:name,image:image};
+    campground.push(camps);
+    res.redirect('/campgrounds');
+});
 app.listen(5500,function(){
     console.log("The YalpCamp Server Started");
 });
